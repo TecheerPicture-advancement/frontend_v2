348e5c82b63dc9a9824d39e2ec3e8f70aed36c76
0in2 <onezeroglgl@gmail.com>
2024-07-09 17:28:49 +0900

    feat: NavBar 구현

--- server/src/App.tsx
+++ server/src/App.tsx
@@ -1,36 +1,19 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar'
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App: React.FC = () => {
   return (
-    <>
-      <div className="flex justify-center space-x-4">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="text-xl font-PR_BL text-green-Normal ">ChitChat</div>
-      <h1 className="text-3xl font-PR_L text-center mt-4">Vite + React</h1>
-      <div className="card bg-gray-200 p-4 shadow-md rounded-lg mt-6 text-center">
-        <button className="btn btn-primary" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className="mt-4">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs mt-6 text-center">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/"  />
+          <Route path="/banner" />
+          <Route path="/background"  />
+          <Route path="/about"  />
+        </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
