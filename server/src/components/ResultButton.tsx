3ce405911c3f83c5b91f689e8a07341e87290ca2
ks9058 <tjdrnr614@naver.com>
2024-07-11 03:46:46 +0900

    feat: MainButton 구현, ResultButton 수정

--- server/src/components/ResultButton.tsx
+++ server/src/components/ResultButton.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 const ResultButton:React.FC<ButtonProps> = (props) =>  {
   return (
-  <button className="flex justify-center items-center min-text-xl-w px-[90px] py-[10px] rounded-[10px] border-2 border-green-Light hover:border-green-Normal active:border-green hover:bg-green-Normal :hover active:bg-green-Normal :active max-text-xl font-PR_M text-center text-green-Light hover:text-black active:text-black" >
+  <button className="flex justify-center items-center px-[90px] py-[10px] rounded-[10px] border-2 border-green-Light hover:border-green-Normal :hover active:border-green-Normal :active hover:bg-green-Normal :hover hover:font-PR_BO active:font-PR_BO active:bg-green-Normal :active text-max-xl font-PR_M text-center text-green-Light hover:text-black active:text-black" >
   {props.value}</button>
 );
 };
