a33513577aa199c5b94ad0e51f94d5932dd3adaa
Gwanghyeon-k <hsys06207@gmail.com>
2024-07-11 23:37:45 +0900

    feat: RadioButton 구현

--- server/src/App.tsx
+++ server/src/App.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar'
+import RadioButton from './components/RadioButton';
 
 const App: React.FC = () => {
+  const [checked, setChecked] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
+  const handleToggle = () => {
+    setChecked(prevChecked => !prevChecked);
+  };
+
+  const handleResetHover = () => {
+    setHovered(false);
+  };
+
+  const handleHover = () => {
+    setHovered(true);
+  };
+
   return (
-    <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/"  />
-          <Route path="/banner" />
-          <Route path="/background"  />
-          <Route path="/about"  />
-        </Routes>
-    </Router>
+    <div>
+      <RadioButton 
+        checked={checked} 
+        hovered={hovered}
+        onToggle={handleToggle} 
+        onResetHover={handleResetHover}
+        onMouseEnter={handleHover}
+      />
+    </div>
   );
 };
 
 export default App;
+
+  // return (
+  //   <Router>
+  //     <div>
+  //       <RadioButton />
+  //     </div>
+  //       {/* <NavBar />
+  //       <Routes>
+  //         <Route path="/"  />
+  //         <Route path="/banner" />
+  //         <Route path="/background"  />
+  //         <Route path="/about"  />
+  //       </Routes> */}
+  //   </Router>
