785d2bdbcf3c52b608671e5e7054f8003c0fd6fc
0in2 <onezeroglgl@gmail.com>
2024-07-12 14:29:23 +0900

    feat: BannerSetting 페이지와 NavBar 동적 수정 완료

--- server/src/App.tsx
+++ server/src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import ResultButton from './components/ResultButton';
+import NavBar from './components/NavBar'
+import BannerSetting from './pages/BannerSetting';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <NavBar/>
-      <Routes>
-        <Route path="/" />
-        <Route path="/banner" />
-        <Route path="/background" />
-        <Route path="/about" />
-      </Routes>
-      <ResultButton value="(문자열을 넣어주세요)"/>
+        <NavBar />
+        <Routes>
+          <Route path="/"  />
+          <Route path="/banner" />
+          <Route path="/background"  />
+          <Route path="/about"  />
+        </Routes>
+        <BannerSetting />
     </Router>
   );
-};
+}
 
 export default App;
---
+++ server/src/components/types/formTypes.ts
@@ -0,0 +1,9 @@
+export interface FormData {
+  productName: string;
+  productConcept: string;
+  productCategory: string;
+  additionalInfo: string;
+  width: number;
+  height: number;
+  aspectRatio: string;
+}
