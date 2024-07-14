720704004a98cea0eeb9caf28ed5d4f4cfac15cf
0in2 <onezeroglgl@gmail.com>
2024-07-15 02:38:32 +0900

    feat:온보딩페이지4 퍼블리싱 완료

--- server/src/App.tsx
+++ server/src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BannerEdit from './pages/BannerEdit';
+import Onboarding3 from './components/Onboarding3';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<BannerEdit mainText1='1234' mainText2='4567' subText1='789' subText2='123456789'/>} />
+        <Route path='/' />
         <Route path="/banner" element={<div>Banner Page</div>} />
         <Route path="/background" element={<div>Background Page</div>} />
         <Route path="/about" element={<div>About Page</div>} />
       </Routes>
+      <Onboarding3/>
     </Router>
   );
 };
