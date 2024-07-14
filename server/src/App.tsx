092d992f88983af68e51914107798df312d796de
Gwanghyeon-k <hsys06207@gmail.com>
2024-07-15 02:53:17 +0900

    feat: BannerResult 퍼블리싱

--- server/src/App.tsx
+++ server/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BannerEdit from './pages/BannerEdit';
+import BannerResult from './pages/BannerResult';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<BannerEdit mainText1='1234' mainText2='4567' subText1='789' subText2='123456789'/>} />
+        <Route path='/' element={<BannerResult />}/>
         <Route path="/banner" element={<div>Banner Page</div>} />
         <Route path="/background" element={<div>Background Page</div>} />
         <Route path="/about" element={<div>About Page</div>} />
