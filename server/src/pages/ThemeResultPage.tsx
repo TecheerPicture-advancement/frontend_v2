4bcfc3fe9724efb7cd2f898dba93164afecb5d6b
ks9058 <tjdrnr614@naver.com>
2024-07-17 22:44:20 +0900

    feat: ThemeResult 페이지 수정

--- server/src/pages/ThemeResultPage.tsx
+++ server/src/pages/ThemeResultPage.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import ImageBox from '../components/ImageBox';
 import ThemeBox from '../components/ThemeBox';
-import MainButton from '../components/MainButton';
+import NavBar from '../components/NavBar';
 
 const ThemeResult: React.FC = () => {
   const [selectedTheme, setSelectedTheme] = useState({
@@ -11,153 +11,172 @@ const ThemeResult: React.FC = () => {
     theme: '',
     detail: '',
   });
+  const [isThemeSelected, setIsThemeSelected] = useState(false);
 
   const handleThemeSelect = (themeDetails: { imageSource: string; detail: string; theme: string }) => {
     setSelectedTheme({
       imageSource: themeDetails.imageSource,
-      prompt: '원하는 사진을 만들어드립니다.', // Assuming prompt remains the same
+      prompt: '원하는 사진을 만들어드립니다.',
       theme: themeDetails.theme,
       detail: themeDetails.detail,
     });
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    setIsThemeSelected(true);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }, 1200); // Adjust this value to control the scroll speed
   };
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-start p-2.5 bg-[#111] overflow-hidden">
+    <div className="bg-black overflow-hidden">
+      <div className="flex-shrink-0 flex-grow-0">
+        <NavBar />
+      </div>
       <div className="w-full max-w-screen-2xl h-full flex flex-col items-center justify-start gap-10 md:gap-20 lg:gap-[90px] overflow-hidden">
-        <header className="h-30 md:h-52 lg:h-[200px] flex flex-col items-center justify-start gap-2.5 md:gap-3 lg:gap-[10px] pt-6 md:pt-8 lg:pt-[23px] pr-6 md:pr-8 lg:pr-[119px] pl-6 md:pl-8 lg:pl-[120px] overflow-hidden">
-          <h1 className="text-xl md:text-2xl lg:text-[40px] leading-tight font-['Noto_Sans_KR'] font-black text-center text-[#fff]">
+        <header className="h-30 mb-10 md:h-52 lg:h-[200px] flex flex-col items-center justify-start gap-2.5 md:gap-3 lg:gap-[10px] pt-6 md:pt-8 lg:pt-[23px] pr-6 md:pr-8 lg:pr-[119px] pl-6 md:pl-8 lg:pl-[120px] overflow-hidden">
+          <h1 className="text-3xl md:text-2xl lg:text-[40px] leading-tight font-PR_BL text-center text-[#fff]">
             내 마음대로 만드는<br />
-            <span className="text-[#00d54b]">상품 이미지</span>
+            <span className="text-green-Normal">상품 이미지</span>
           </h1>
-          <p className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[270px] h-[32px] relative gap-3 px-3 rounded-[10px]">
-            <MainButton value="이미지 업로드 하러 가기"/>
-          </p>
+          <span className="text-center text-white font-PR_L">원하는 상품 이미지를 클릭해주세요</span>
         </header>
-        <motion.div
-          key={selectedTheme.theme}
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -50 }}
-        >
-          <ThemeBox
-            imageSource={selectedTheme.imageSource}
-            prompt={selectedTheme.prompt}
-            theme={selectedTheme.theme}
-            detail={selectedTheme.detail}
-          />
-        </motion.div>
-        <main className="flex flex-col items-center justify-center w-full">
-          <div className="relative w-full max-w-4xl h-[230px] shrink-0 flex items-center justify-center">
-            {selectedTheme.theme !== '도시' && (
-              <ImageBoxPosition
-                className="left-[75%] top-[140px]"
-                imageSource="ThemeImage/city.png"
-                detail="차갑고 세련된"
-                theme="도시"
-                onClick={handleThemeSelect}
+        <AnimatePresence>
+          {isThemeSelected && (
+            <motion.div
+              key={selectedTheme.theme}
+              layoutId={selectedTheme.theme}
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+            >
+              <ThemeBox
+                imageSource={selectedTheme.imageSource}
+                prompt={selectedTheme.prompt}
+                theme={selectedTheme.theme}
+                detail={selectedTheme.detail}
               />
-            )}
-            {selectedTheme.theme !== '자연' && (
-              <ImageBoxPosition
-                className="left-[50%] top-0"
-                imageSource="ThemeImage/natural.png"
-                detail="편안하고 포근한"
-                theme="자연"
-                onClick={handleThemeSelect}
-              />
-            )}
-            {selectedTheme.theme !== '명소' && (
-              <ImageBoxPosition
-                className="left-[25%] top-[140px]"
-                imageSource="ThemeImage/place.png"
-                detail="아름답고 현실적인"
-                theme="명소"
-                onClick={handleThemeSelect}
-              />
-            )}
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <main className="flex flex-row items-center justify-center w-full h-full gap-5">
+          <div className="relative w-[200px] max-w-4xl h-[900px] shrink-0 flex items-center justify-center">
             {selectedTheme.theme !== 'AI' && (
               <ImageBoxPosition
-                className="left-0 top-0 lg:w-[280px] lg:h-[280px]"
+                className="bottom-[75%] lg:w-[280px] lg:h-[280px] font-PR_M"
                 imageSource="ThemeImage/AI.png"
                 detail="적합한 이미지 추천"
                 theme="AI"
                 onClick={handleThemeSelect}
               />
             )}
-          </div>
-          <div className="relative w-full max-w-4xl h-[230px] shrink-0 flex items-center justify-center">
-            {selectedTheme.theme !== '겨울' && (
-              <ImageBoxPosition
-                className="left-[75%] top-[140px]"
-                imageSource="ThemeImage/winter.png"
-                detail="얼어붙은 나무들"
-                theme="겨울"
-                onClick={handleThemeSelect}
-              />
-            )}
-            {selectedTheme.theme !== '가을' && (
-              <ImageBoxPosition
-                className="left-[50%] top-0"
-                imageSource="ThemeImage/fall.png"
-                detail="낭만적인 단풍잎"
-                theme="가을"
-                onClick={handleThemeSelect}
-              />
-            )}
-            {selectedTheme.theme !== '여름' && (
-              <ImageBoxPosition
-                className="left-[25%] top-[140px]"
-                imageSource="ThemeImage/summer.png"
-                detail="시원한 바닷가"
-                theme="여름"
-                onClick={handleThemeSelect}
-              />
-            )}
             {selectedTheme.theme !== '봄' && (
               <ImageBoxPosition
-                className="left-0 top-0 lg:w-[280px] lg:h-[280px]"
+                className="bottom-[50%] lg:w-[280px] lg:h-[280px] font-PR_M"
                 imageSource="ThemeImage/spring.png"
                 detail="흩날리는"
                 theme="봄"
                 onClick={handleThemeSelect}
               />
             )}
-          </div>
-          <div className="relative w-full max-w-4xl h-[400px] shrink-0 flex items-center justify-center">
-            {selectedTheme.theme !== '직접입력' && (
+            {selectedTheme.theme !== '스튜디오' && (
               <ImageBoxPosition
-                className="left-[75%] top-[140px] w-56 h-56 md:w-64 md:h-64"
-                imageSource="ThemeImage/pen.png"
-                detail="내가 원하는 테마가 없다면?"
-                theme="직접입력"
+                className="bottom-[25%] lg:w-[280px] lg:h-[280px] font-PR_M"
+                imageSource="ThemeImage/studio.png"
+                detail="감성적인 디자인"
+                theme="스튜디오"
                 onClick={handleThemeSelect}
               />
             )}
-            {selectedTheme.theme !== '카페' && (
+          </div>
+          <div className="relative w-[200px] max-w-4xl h-[900px] shrink-0 flex items-center justify-center">
+            {selectedTheme.theme !== '명소' && (
               <ImageBoxPosition
-                className="left-[50%] top-0 w-56 h-56 md:w-64 md:h-64"
-                imageSource="ThemeImage/cafe.png"
-                detail="모던한 디자인"
-                theme="카페"
+                className="bottom-[62.5%] lg:w-[280px] lg:h-[280px] font-PR_M"
+                imageSource="ThemeImage/place.png"
+                detail="아름답고 현실적인"
+                theme="명소"
+                onClick={handleThemeSelect}
+              />
+            )}
+            {selectedTheme.theme !== '여름' && (
+              <ImageBoxPosition
+                className="bottom-[37.5%] lg:w-[280px] lg:h-[280px] font-PR_M"
+                imageSource="ThemeImage/summer.png"
+                detail="시원한 바닷가"
+                theme="여름"
                 onClick={handleThemeSelect}
               />
             )}
             {selectedTheme.theme !== '오피스' && (
               <ImageBoxPosition
-                className="left-[25%] top-[140px] w-56 h-56 md:w-64 md:h-64"
+                className="bottom-[12.5%] lg:w-[280px] lg:h-[280px] font-PR_M"
                 imageSource="ThemeImage/office.png"
                 detail="정돈된 환경"
                 theme="오피스"
                 onClick={handleThemeSelect}
               />
             )}
-            {selectedTheme.theme !== '스튜디오' && (
+          </div>
+          <div className="relative w-[200px] max-w-4xl h-[900px] shrink-0 flex items-center justify-center">
+            {selectedTheme.theme !== '자연' && (
               <ImageBoxPosition
-                className="left-0 top-0 w-56 h-56 md:w-64 md:h-64 lg:w-[280px] lg:h-[280px]"
-                imageSource="ThemeImage/studio.png"
-                detail="감성적인 디자인"
-                theme="스튜디오"
+                className="bottom-[75%] lg:w-[280px] lg:h-[280px] font-PR_M"
+                imageSource="ThemeImage/natural.png"
+                detail="편안하고 포근한"
+                theme="자연"
+                onClick={handleThemeSelect}
+              />
+            )}
+            {selectedTheme.theme !== '가을' && (
+              <ImageBoxPosition
+                className="bottom-[50%] lg:w-[280px] lg:h-[280px] font-PR_M"
+                imageSource="ThemeImage/fall.png"
+                detail="낭만적인 단풍잎"
+                theme="가을"
+                onClick={handleThemeSelect}
+              />
+            )}
+            {selectedTheme.theme !== '카페' && (
+              <ImageBoxPosition
+                className="bottom-[25%] lg:w-[280px] lg:h-[280px] font-PR_M"
+                imageSource="ThemeImage/cafe.png"
+                detail="모던한 디자인"
+                theme="카페"
+                onClick={handleThemeSelect}
+              />
+            )}
+          </div>
+          <div className="relative w-[200px] max-w-4xl h-[900px] shrink-0 flex items-center justify-center">
+            {selectedTheme.theme !== '도시' && (
+              <ImageBoxPosition
+                className="bottom-[62.5%] lg:w-[280px] lg:h-[280px] font-PR_M"
+                imageSource="ThemeImage/city.png"
+                detail="차갑고 세련된"
+                theme="도시"
+                onClick={handleThemeSelect}
+              />
+            )}
+            {selectedTheme.theme !== '겨울' && (
+              <ImageBoxPosition
+                className="bottom-[37.5%] lg:w-[280px] lg:h-[280px] font-PR_M"
+                imageSource="ThemeImage/winter.png"
+                detail="얼어붙은 나무들"
+                theme="겨울"
+                onClick={handleThemeSelect}
+              />
+            )}
+            {selectedTheme.theme !== '직접입력' && (
+              <ImageBoxPosition
+                className="bottom-[12.5%] lg:w-[280px] lg:h-[280px] font-PR_M"
+                imageSource="ThemeImage/pen.png"
+                detail="내가 원하는 테마가 없다면?"
+                theme="직접입력"
                 onClick={handleThemeSelect}
               />
             )}
@@ -183,7 +202,7 @@ const ImageBoxPosition: React.FC<ImageBoxPositionProps> = ({ className, imageSou
       onClick={() => onClick({ imageSource, detail, theme })}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
-      layout
+      layoutId={theme}
     >
       <ImageBox imageSource={imageSource} detail={detail} theme={theme} />
     </motion.div>
