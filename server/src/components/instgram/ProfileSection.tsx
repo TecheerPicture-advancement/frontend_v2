9840904c962630302f85c4e49e0506dfc0e5ee24
0in <onezeroglgl@gmail.com>
2025-01-22 02:41:36 +0900

    design:Adjust profile picture aspect ratio

--- server/src/components/instgram/ProfileSection.tsx
+++ server/src/components/instgram/ProfileSection.tsx
@@ -5,11 +5,11 @@ const ProfileSection: React.FC = () => {
         <section className="flex flex-col justify-start items-start h-[507px] w-[380px] relative">
             <div className="flex justify-start items-center absolute gap-2.5 pl-[30px] pr-10 py-[30px] z-10">
                 <img
-                    className="w-12 h-12 rounded-full object-cover"
+                    className="w-10 h-10 rounded-full object-cover"
                     src="../../public/assets/logo.png"
                     alt="profile"
                 />
-                <span className="text-lg font-bold text-white">TecheerPicture</span>
+                <span className="text-base font-bold text-white">TecheerPicture</span>
             </div>
             <div className="relative w-full h-full">
                 <div
